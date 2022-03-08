f7f1f6d1b56bb700e9d4206322f0fc26f03f40e0
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-08 15:31:28 +0100

    Get post from slug

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,11 +1,9 @@
 <x-layout>
-
         @foreach ($posts as $post)
-
             <article>
                 <h1>
-                    <a href="/posts/{{ $post->id}}">
-                        {{$post->title}}
+                    <a href="/posts/{{ $post->slug}}">
+                        {!! $post->title!!}
                     </a>
                 </h1>
                 <div>
