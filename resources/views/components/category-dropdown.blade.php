640558b9645eb829d393bb29299e82cd55065ff8
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-10 15:28:30 +0100

    Added Pager

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -10,13 +10,13 @@
         </x-slot>
 
 
-        <x-dropdown-item href="/" :active="request()->routeIS('home')">
+        <x-dropdown-item href="/?{{ http_build_query(request()->except('category', 'page')) }}" :active="request()->routeIS('home')">
             All
         </x-dropdown-item>
 
 
         @foreach($categories as $category)
-            <x-dropdown-item href="/?category={{$category->slug}}&{{ http_build_query(request()->except('category')) }}"
+            <x-dropdown-item href="/?category={{$category->slug}}&{{ http_build_query(request()->except('category', 'page')) }}"
                              :active='request()->is("categories/{$category->slug}")'>
                 {{ucwords($category->name)}}
             </x-dropdown-item>
