a3a56bab341495cdb97616e47d087c7f9d63b875
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-10 14:11:45 +0100

    Refactored search with controllers

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,5 +23,12 @@ class Post extends Model
             $query
                 -> where('title', 'like', '%'.request('search').'%')
                 -> orWhere('body', 'like', '%'.request('search').'%'));
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query
+                ->whereHas('category', fn($query) =>
+                    $query-> where('slug', $category)
+                )
+        );
     }
 }
