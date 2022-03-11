348e5461298f942d354fdae747457e36e35aecd8
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-11 10:46:48 +0100

    Added succesful creation flash msg

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -12,8 +12,12 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{old('name')}}"
                            required
                     >
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class ="mb-6 " >
@@ -24,8 +28,14 @@
                             type="text"
                             name="username"
                             id="username"
+                           value="{{old('username')}}"
                             required
                     >
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
+
+
                 </div>
 
                 <div class ="mb-6 " >
@@ -36,6 +46,7 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{old('email')}}"
                            required
                     >
                     @error('email')
@@ -53,6 +64,9 @@
                            id="password"
                            required
                     >
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
