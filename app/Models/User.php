f3e6c6d063a2cdc034dd5b5a5076b6cbb3b55278
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-11 09:33:45 +0100

    Added register for user creation, and added mutator to keep passwords hashed

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -47,4 +43,8 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
